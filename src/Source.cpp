2a843db8a5d9ae52e6456f32609c307f6d069ec5
michals96 <michal.stefaniuk9@gmail.com>
2020-11-23 23:46:23 +0100

    Add orthogonal and perspective projections

--- src/Source.cpp
+++ src/Source.cpp
@@ -14,7 +14,8 @@ struct Spherical
 };
 
 Spherical camera(3.0f, 0.2f, 1.2f);
-sf::Vector3f pos(0.0f, 0.0f, 0.0f), scale(1.0f, 1.0f, 1.0f), rot(0.0f, 0.0f, 0.0f); //new
+sf::Vector3f pos(0.0f, 0.0f, 0.0f), scale(1.0f, 1.0f, 1.0f), rot(0.0f, 0.0f, 0.0f);
+unsigned char projection_type = 'p'; //new
 
 void initOpenGL(void)
 {
@@ -27,7 +28,8 @@ void reshapeScreen(sf::Vector2u size)
     glViewport(0, 0, (GLsizei)size.x, (GLsizei)size.y);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (GLdouble)size.x / (GLdouble)size.y, 0.1, 100.0);
+    if (projection_type == 'p') gluPerspective(45.0, (GLdouble)size.x / (GLdouble)size.y, 0.1, 100.0); //new
+    else glOrtho(-1.245 * ((GLdouble)size.x / (GLdouble)size.y), 1.245 * ((GLdouble)size.x / (GLdouble)size.y), -1.245, 1.245, -3.0, 12.0); //new
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -57,12 +59,11 @@ void drawScene()
     glEnd();
     glDisable(GL_LINE_STIPPLE);
 
-    //transformacje
-    glTranslatef(pos.x, pos.y, pos.z);   //new
-    glRotatef(rot.x, 1, 0, 0);           //new
-    glRotatef(rot.y, 0, 1, 0);           //new
-    glRotatef(rot.z, 0, 0, 1);           //new
-    glScalef(scale.x, scale.y, scale.z); //new
+    glTranslatef(pos.x, pos.y, pos.z);
+    glRotatef(rot.x, 1, 0, 0);
+    glRotatef(rot.y, 0, 1, 0);
+    glRotatef(rot.z, 0, 0, 1);
+    glScalef(scale.x, scale.y, scale.z);
 
     glLineWidth(2.0);
     glColor3f(0, 0, 0);
@@ -101,22 +102,27 @@ int main()
         {
             if (event.type == sfe::Closed || (event.type == sfe::KeyPressed && event.key.code == sfk::Escape)) running = false;
             if (event.type == sfe::Resized) reshapeScreen(window.getSize());
+            if (event.type == sf::Event::KeyPressed) //new
+            {
+                if (event.key.code == sf::Keyboard::Num0) { projection_type = 'o'; reshapeScreen(window.getSize()); } //new
+                if (event.key.code == sf::Keyboard::Num9) { projection_type = 'p'; reshapeScreen(window.getSize()); } //new
+            }
         }
         if (sfk::isKeyPressed(sfk::Left)) camera.fi -= 0.01f;
         if (sfk::isKeyPressed(sfk::Right)) camera.fi += 0.01f;
         if (sfk::isKeyPressed(sfk::Up)) camera.theta += 0.01f;
         if (sfk::isKeyPressed(sfk::Down)) camera.theta -= 0.01f;
 
-        if (sfk::isKeyPressed(sfk::LShift)) shift_key_state = -1;        //new
-        if (sfk::isKeyPressed(sfk::Q)) pos.x += 0.01f * shift_key_state;   //new
-        if (sfk::isKeyPressed(sfk::A)) pos.y += 0.01f * shift_key_state;   //new
-        if (sfk::isKeyPressed(sfk::Z)) pos.z += 0.01f * shift_key_state;   //new
-        if (sfk::isKeyPressed(sfk::W)) scale.x += 0.01f * shift_key_state; //new
-        if (sfk::isKeyPressed(sfk::S)) scale.y += 0.01f * shift_key_state; //new
-        if (sfk::isKeyPressed(sfk::X)) scale.z += 0.01f * shift_key_state; //new
-        if (sfk::isKeyPressed(sfk::E)) rot.x += 0.5f * shift_key_state;    //new
-        if (sfk::isKeyPressed(sfk::D)) rot.y += 0.5f * shift_key_state;    //new
-        if (sfk::isKeyPressed(sfk::C)) rot.z += 0.5f * shift_key_state;    //new
+        if (sfk::isKeyPressed(sfk::LShift)) shift_key_state = -1;
+        if (sfk::isKeyPressed(sfk::Q)) pos.x += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::A)) pos.y += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::Z)) pos.z += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::W)) scale.x += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::S)) scale.y += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::X)) scale.z += 0.01f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::E)) rot.x += 0.5f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::D)) rot.y += 0.5f * shift_key_state;
+        if (sfk::isKeyPressed(sfk::C)) rot.z += 0.5f * shift_key_state;
         shift_key_state = 1;
 
         drawScene();
